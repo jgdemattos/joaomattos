a34fd549e99bed0b8bde04c0e142f2d36b3e4a28
Joao <jgdemattos@hotmail.com>
2022-12-07 10:11:16 -0300

    fix: gatsby-source-filesystem required to work with manifest, added and configured

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
     {
@@ -27,6 +27,13 @@ module.exports = {
         host: "https://www.joaomattos.com",
         sitemap: "https://www.joaomattos.com/sitemap-index.xml",
         policy: [{ userAgent: "*", allow: "/" }],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
       },
     },
     "gatsby-plugin-image",
