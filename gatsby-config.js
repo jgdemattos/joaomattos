2127bdd5e812e920305f5b573e600626e6a72bd8
Joao <jgdemattos@hotmail.com>
2022-12-07 09:29:22 -0300

    feat: added manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,21 @@ module.exports = {
   siteMetadata: {
     siteUrl: `https://www.joaomattos.com`,
   },
-  plugins: ['gatsby-plugin-postcss',],
+  plugins: [
+    "gatsby-plugin-postcss",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `João Mattos's Blog`,
+        short_name: `joaomattos`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+  ],
 }
