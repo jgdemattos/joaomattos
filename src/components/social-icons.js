48a7a54064e14deeb331b8ba5bb5c8708595e6d7
Joao Mattos <joao.caramelo.studio@gmail.com>
2024-01-20 11:30:19 -0300

    feat: new social icons

--- src/components/social-icons.js
+++ src/components/social-icons.js
@@ -3,31 +3,10 @@ import React from 'react'
 function SocialIcons() {
   return (
     <div>
-      <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-        <a className="text-gray-400">
-          <svg
-            fill="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            className="w-5 h-5"
-            viewBox="0 0 24 24"
-          >
-            <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-          </svg>
-        </a>
-        <a className="ml-3 text-gray-400">
-          <svg
-            fill="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            className="w-5 h-5"
-            viewBox="0 0 24 24"
-          >
-            <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-          </svg>
-        </a>
+      <span
+        className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start"
+        href="https://www.instagram.com/mattos.pro/"
+      >
         <a className="ml-3 text-gray-400">
           <svg
             fill="none"
@@ -42,7 +21,10 @@ function SocialIcons() {
             <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
           </svg>
         </a>
-        <a className="ml-3 text-gray-400">
+        <a
+          className="ml-3 text-gray-400"
+          href="https://www.linkedin.com/in/joaovieiramattos/"
+        >
           <svg
             fill="currentColor"
             stroke="currentColor"
@@ -56,6 +38,26 @@ function SocialIcons() {
               stroke="none"
               d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
             ></path>
+            <circle cx="4" cy="4" r="2" stroke="none"></circle>
+          </svg>
+        </a>
+        <a
+          className="ml-3 text-gray-400"
+          href="https://www.youtube.com/@JoaoMattosPro"
+        >
+          <svg
+            fill="currentColor"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="0"
+            className="w-5 h-5"
+            viewBox="-271 311.2 256 179.8"
+          >
+            <path
+              d="M-59.1,311.2h-167.8c0,0-44.1,0-44.1,44.1v91.5c0,0,0,44.1,44.1,44.1h167.8c0,0,44.1,0,44.1-44.1v-91.5
+	C-15,355.3-15,311.2-59.1,311.2z M-177.1,450.3v-98.5l83.8,49.3L-177.1,450.3z"
+            />
             <circle cx="4" cy="4" r="2" stroke="none"></circle>
           </svg>
         </a>
